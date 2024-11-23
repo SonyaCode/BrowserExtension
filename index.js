cd5118d7c66dece612015fb6012ac17158b403d7
SonyaCode <Sonya.Zh2018@gmail.com>
2024-11-23 17:16:16 -0500

    Finished

--- index.js
+++ index.js
@@ -1,13 +1,55 @@
 let inputEl = document.getElementById("input-el")
-let button = document.getElementById("input-btn")
+let inputBtn = document.getElementById("input-btn")
+let saveBtn = document.getElementById("save-btn")
+let deleteBtn = document.getElementById("delete-btn")
 const ulEl = document.getElementById("ul-el")
 
-button.addEventListener("click", renderLeads)
+
 let myLeads = []
+let leadsFromLocalStorage = localStorage.getItem("myLeads")
+// if there is already a localStorage, retrieve and store the data to myLeads
+if (leadsFromLocalStorage) {
+    myLeads = JSON.parse(leadsFromLocalStorage) // list
+    renderLeads()
+} else { // create a localStorage
+    localStorage.setItem("myLeads", JSON.stringify(myLeads)) // string
+
+}
+
+inputBtn.addEventListener("click", function() {
+    myLeads.push(inputEl.value)
+    inputEl.value = ""
+    // Save the myLeads array to localStorage
+    localStorage.setItem("myLeads", JSON.stringify(myLeads));
+    renderLeads()
+   
+    // To verify that it works:
+    console.log(localStorage.getItem("myLeads") )
+})
+
+// get link of the current tab URL
+saveBtn.addEventListener("click", function() {
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+        let tabURL = tabs[0].url;
+        myLeads.push(tabURL) // add the tab's URL to the list
+        localStorage.setItem("myLeads", JSON.stringify(myLeads));
+        renderLeads() // render it so it appears
+
+        console.log(tabURL)
+    });
+})
+
+// delete all the saved links
+deleteBtn.addEventListener("click", function() {
+    myLeads = [] // empty the list
+    localStorage.clear()
+    localStorage.setItem("myLeads", JSON.stringify(myLeads));
+    renderLeads() // render it so it appears
+})
 
 function renderLeads() {
-    myLeads.push(inputEl.value)
-    
+    leadsFromLocalStorage = localStorage.getItem("myLeads")
+    myLeads = JSON.parse(leadsFromLocalStorage)
     let listItems = ""
     for (let i = 0; i < myLeads.length; i++) {
         listItems += `
@@ -16,6 +58,4 @@ function renderLeads() {
         </li>`
     }
     ulEl.innerHTML = listItems
-
-    inputEl.value = ""
 }
